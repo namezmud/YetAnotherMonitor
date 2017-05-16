b419fea34244af82987c0bfd94af87b6756f197c
namezmud <drtroake@gmail.com>
2017-05-16 10:55:33 +1000

    Switched output to logging module

--- YAMprocess_sensors.py
+++ YAMprocess_sensors.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 import re
 import datetime
 import json
+import logging
 
 #
 # sensor_list.json File Format
@@ -19,11 +20,12 @@ def ts_ms(d):
 
 # The callback when client receives a CANNACK from server
 def on_connect(client, userdata, flags, rc):
-    print("Connected : " + str(rc))
+    logging.info("Connected : " + str(rc))
 
     #subscribe on connect so we resubscibe if it is lost and reconnects
     client.subscribe("YarraRiverReptiles/#")
 
+last = {"temperature":{}, "flow":{}}
 
 #The callback when PUBLISH received from server
 def on_message(client, userdata, msg):
@@ -36,32 +38,40 @@ def on_message(client, userdata, msg):
                 sensor = ma.group(1)
                 temperature = ma.group(2)
                 name = sensor_list[sensor]
-                
-                data = {"name" : name,
-                        "time_ms" : ts_ms(time_now),
-                        "time_str" : time_now.strftime('%Y-%m-%d %H%M%S'),
-                        "temperature" : temperature,
-                        "ID" : sensor}
-                print(json.dumps(data))
-                client.publish("YarraRiverReptiles/tanks/"+name+"/temperature", json.dumps(data))
+                if name not in last["temperature"] or last["temperature"][name] < time_now - datetime.timedelta(minutes=4, seconds=50):
+                    data = {"name" : name,
+                            "time_ms" : ts_ms(time_now),
+                            "time_str" : time_now.strftime('%Y-%m-%d %H%M%S'),
+                            "temperature" : temperature,
+                            "ID" : sensor}
+                    logging.info(json.dumps(data))
+                    client.publish("YarraRiverReptiles/tanks/"+name+"/temperature", json.dumps(data))
+                    last["temperature"][name] = time_now
             else:
-                print("Not found")
+                logging.debug("Not found")
     elif re.search(r'data/flow', msg.topic):
         ma = re.search(r'Pin(.*) =(.*)L', msg.payload.decode("utf-8"))
         if ma:
+                
             rate = temperature = ma.group(2)
             name = "Turtle Tank"
-            data = {"name" : name,
-                    "time_ms" : ts_ms(time_now),
-                    "time_str" : time_now.strftime('%Y-%m-%d %H%M%S'),
-                    "flow" : rate,
-                    "ID"   : ma.group(1)}
-            print(json.dumps(data))
-            client.publish("YarraRiverReptiles/tanks/"+name+"/flow", json.dumps(data))
+            
+            if name not in last["flow"] or last["flow"][name] < time_now - datetime.timedelta(minutes=4, seconds=50):
+
+                data = {"name" : name,
+                        "time_ms" : ts_ms(time_now),
+                        "time_str" : time_now.strftime('%Y-%m-%d %H%M%S'),
+                        "flow" : rate,
+                        "ID"   : ma.group(1)}
+                logging.info(json.dumps(data))
+                client.publish("YarraRiverReptiles/tanks/"+name+"/flow", json.dumps(data))
+                last["flow"][name] = time_now
+                
         else:
-                print("Flow fail")
+                logging.debug("Flow fail")
 #    print(msg.topic+" "+str(msg.payload))
 
+logging.basicConfig(format="%(asctime)s %(message)s", filename="YAM.log", level=logging.DEBUG)
 
 client = mqtt.Client()
 client.on_connect = on_connect
